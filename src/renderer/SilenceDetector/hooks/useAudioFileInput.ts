fcab1a3bc54d43016caf16d93244785d8443e79b
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-05-02 21:07:41 -0700

    chore: Move creating optimized audio file message to the hook

--- src/renderer/SilenceDetector/hooks/useAudioFileInput.ts
+++ src/renderer/SilenceDetector/hooks/useAudioFileInput.ts
@@ -1,4 +1,6 @@
 import { Dispatch, SetStateAction } from 'react';
+import { message } from 'antd';
+import { CREATE_OPTIMIZED_AUDIO_FILE } from 'renderer/messages';
 
 export function useAudioFileInput(
   setInputFile: Dispatch<SetStateAction<File | null>>,
@@ -6,6 +8,13 @@ export function useAudioFileInput(
   setIntervals: Dispatch<SetStateAction<Array<any>>>
 ) {
   return (file: File) => {
+    message.open({
+      key: CREATE_OPTIMIZED_AUDIO_FILE,
+      type: 'loading',
+      content: 'Creating optimized audio file...',
+      duration: 0,
+    });
+
     setInputFile(file);
     setIsLoading(true);
     setIntervals([]);
