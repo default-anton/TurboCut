b29dc2c271caad727ce450641b6064c8f24439d2
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-06-10 13:43:26 -0700

    feat: split text into paragraphs

--- src/renderer/components/TranscriptionEditor.tsx
+++ src/renderer/components/TranscriptionEditor.tsx
@@ -1,10 +1,10 @@
-import { forwardRef } from 'react';
+import { forwardRef, useMemo } from 'react';
 import { Card, Typography } from 'antd';
 
 import { Transcription } from 'shared/types';
 import styles from './TranscriptionEditor.module.scss';
 
-const { Text } = Typography;
+const { Text, Paragraph } = Typography;
 
 interface Props {
   transcription: Transcription;
@@ -13,22 +13,55 @@ interface Props {
 }
 
 const TranscriptionEditor = forwardRef<HTMLElement, Props>(
-  ({ transcription, segmentAtPlayhead, disabledSegmentIds }, ref) => (
-    <Card className={styles.card}>
-      {transcription.map(({ id, text }) => (
-        <Text
-          ref={id === segmentAtPlayhead ? ref : null}
-          key={id}
-          delete={disabledSegmentIds.has(id)}
-          data-segment-id={id}
-          className={styles.text}
-          mark={id === segmentAtPlayhead}
-        >
-          {text}
-        </Text>
-      ))}
-    </Card>
-  )
+  ({ transcription, segmentAtPlayhead, disabledSegmentIds }, ref) => {
+    const meanPauseInterval = useMemo(
+      () =>
+        transcription.reduce(
+          (acc, { start }, index, array) =>
+            index === 0 ? acc : acc + start - array[index - 1].end,
+          0
+        ) / transcription.length,
+      [transcription]
+    );
+
+    return (
+      <Card className={styles.card}>
+        {transcription.map(
+          ({ id, text, start }, index) =>
+            (start - (transcription[index - 1]?.end || 0) >
+              meanPauseInterval && (
+              <Paragraph
+                key={`${id}-gap`}
+                className={styles.gap}
+                data-segment-id={id}
+              >
+                <Text
+                  ref={id === segmentAtPlayhead ? ref : null}
+                  key={id}
+                  delete={disabledSegmentIds.has(id)}
+                  data-segment-id={id}
+                  className={styles.text}
+                  mark={id === segmentAtPlayhead}
+                >
+                  {text}
+                </Text>
+              </Paragraph>
+            )) || (
+              <Text
+                ref={id === segmentAtPlayhead ? ref : null}
+                key={id}
+                delete={disabledSegmentIds.has(id)}
+                data-segment-id={id}
+                className={styles.text}
+                mark={id === segmentAtPlayhead}
+              >
+                {text}
+              </Text>
+            )
+        )}
+      </Card>
+    );
+  }
 );
 
 export default TranscriptionEditor;
