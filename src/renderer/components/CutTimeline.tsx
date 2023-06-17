2a986a81290c45cab8dc1bf1d51e8ae59b1aac10
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-06-16 22:43:25 -0700

    feat: Add change gain slider from 0% to 400%

--- src/renderer/components/CutTimeline.tsx
+++ src/renderer/components/CutTimeline.tsx
@@ -4,7 +4,7 @@ import WaveSurferRegions from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.
 import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min';
 import { RegionParams } from 'wavesurfer.js/src/plugin/regions';
 
-import { theme, FloatButton, Typography } from 'antd';
+import { theme, FloatButton, Typography, Slider } from 'antd';
 import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
 
 import { useProjectConfig } from 'renderer/hooks/useProjectConfig';
@@ -61,6 +61,8 @@ const CutTimeline: FC<CutTimelineProps> = ({
   } = useProjectConfig();
   const [isPlaying, setIsPlaying] = useState(false);
   const [playbackRate, setPlaybackRate] = useState(1);
+  const [gain, setGain] = useState(1);
+  const gainNode = useRef<any>(null);
 
   const [audioFile, setAudioFile] = useState<string | undefined>(undefined);
   const [audioFileDuration, setAudioFileDuration] = useState<
@@ -115,7 +117,7 @@ const CutTimeline: FC<CutTimelineProps> = ({
 
     // Initialize Wavesurfer.js
     waveSurferRef.current = WaveSurfer.create({
-      backend: 'MediaElement',
+      backend: 'MediaElementWebAudio',
       container: waveformRef.current,
       waveColor: token.colorPrimary,
       progressColor: token.colorFillSecondary,
@@ -125,6 +127,10 @@ const CutTimeline: FC<CutTimelineProps> = ({
         WaveSurferRegions.create(),
       ],
     });
+
+    gainNode.current = waveSurferRef.current.backend.ac.createGain();
+    gainNode.current.gain.value = 1;
+    waveSurferRef.current.backend.setFilters([gainNode.current]);
 
     waveSurferRef.current.load(`file://${audioFile}`);
 
@@ -146,6 +152,8 @@ const CutTimeline: FC<CutTimelineProps> = ({
 
     const onAudioPositionChange = () => {
       if (!waveSurferRef.current || skipRegionInProgress.current) return;
+
+      console.log(waveSurferRef.current.getVolume());
 
       const currentTime = waveSurferRef.current.getCurrentTime();
 
@@ -221,6 +229,12 @@ const CutTimeline: FC<CutTimelineProps> = ({
   }, [playbackRate]);
 
   useEffect(() => {
+    if (!waveSurferRef.current) return;
+
+    gainNode.current.gain.value = gain;
+  }, [gain]);
+
+  useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === ' ') {
         event.preventDefault();
@@ -273,6 +287,23 @@ const CutTimeline: FC<CutTimelineProps> = ({
           />
         ))}
       </FloatButton.Group>
+
+      <Slider
+        min={0}
+        max={400}
+        marks={{ 0: '0%', 100: '100%', 200: '200%', 300: '300%', 400: '400%' }}
+        onChange={(value) => setGain(value / 100)}
+        value={gain * 100}
+        style={{
+          position: 'fixed',
+          bottom: token.controlHeightLG,
+          left: `calc(50% + ${token.controlHeightLG * 2}px)`,
+          height: token.controlHeightLG * 10,
+          zIndex: 100,
+          opacity: 1,
+        }}
+        vertical
+      />
     </>
   );
 };
